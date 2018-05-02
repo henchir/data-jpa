463abb94d26fc2936184427d47b4f99295f8b528
Mohamed <Mohamed@192.168.1.46>
2018-05-02 21:14:50 +0200

    commit

---
+++ data-jpa/src/main/java/com/henchir/Main.java
@@ -0,0 +1,12 @@
+package com.henchir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = { "com.henchir" })
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
---
+++ data-jpa/src/main/java/com/henchir/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.henchir.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.henchir.entities.Users;
+
+interface UserRepository extends JpaRepository<Users, Long> {
+
+	Users findByName(String name);
+}
